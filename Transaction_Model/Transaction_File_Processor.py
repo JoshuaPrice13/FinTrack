88fd97c20b5b634fdcd7d6f312d556326bab3b34
Joshua Price <78606903+JoshuaPrice13@users.noreply.github.com>
2025-09-24 09:34:27 -0500

    Create Transaction_File_Processor.py

---
+++ Transaction_Model/Transaction_File_Processor.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from typing import Optional
+
+class File_Type(Enum):
+    TRANSACTION_HISTORY = 0
+    BANK_STATEMENT = 1
+
+
+class TransactionFileProcessor:
+
+    def __init__(self, 
+                 file_type: File_Type, 
+                 #Optional Parameters for Transaction_History type files
+                 csv_bool: Optional[bool] =None,
+                 headers_bool: Optional[bool] =None,
+                 num_of_colums: Optional[int] =None,
+                 
+                 
+                 ):
+        
